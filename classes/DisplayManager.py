4527ce9d63976a04aaebf50d4dd225601f5bbd5b
pottsb <63508539+pottsb@users.noreply.github.com>
2025-04-29 19:27:46 +0100

    comments

--- classes/DisplayManager.py
+++ classes/DisplayManager.py
@@ -1,10 +1,10 @@
 class DisplayManager:
     @staticmethod
-    def display_fail(senseHat):
+    def display_fail(senseHat): # Red dots in bottom corners
         senseHat.set_pixel(7, 7, 254, 0, 0)
         senseHat.set_pixel(0, 7, 254, 0, 0)
 
     @staticmethod
-    def display_success(senseHat):
+    def display_success(senseHat): # Green dots in bottom corners
         senseHat.set_pixel(7, 7, 0, 254, 0)
         senseHat.set_pixel(0, 7, 0, 254, 0)
